63bbf1fec70774fd59d4c43cbb90c54edd32a52e
Fisherman August <fshrmn.august@gmail.com>
2025-07-17 20:29:46 +0300

    added 2 books and book struct

--- server.go
+++ server.go
@@ -10,9 +10,16 @@ import (
 /*
 GET /books - получить все книги
 GET /books/{id} - получить одну книгу с идентификатором
-
 POST /books - отправить (создать) новую книгу
 */
+type Book struct {
+	ID     uint
+	Title  string
+	Author string
+}
+
+var books = []Book{}
+
 func BooksHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Books")
 }
@@ -20,8 +27,9 @@ func AddBookHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Add Book")
 }
 func main() {
+	books = append(books, Book{ID: 1, Title: "Искусство любить", Author: "Эрих Фромм"})
+	books = append(books, Book{ID: 2, Title: "Война и Мир", Author: "Толстой"})
 	r := mux.NewRouter()
-
 	r.HandleFunc("/books", BooksHandler)
 	r.HandleFunc("/books/{id}", AddBookHandler)
 	fmt.Println("СТАРТУЕМ! Я сказал стартуем...")
